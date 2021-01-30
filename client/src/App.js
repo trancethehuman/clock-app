c06b616bfdb516a6f42bbc8dac03adfab2084740
Hai Nghiem <hainghiem375@gmail.com>
2021-01-30 10:22:51 -0330

    Working React Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+// function getTime() {
+//   fetch('http://localhost:3000/time/now')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// }
+
+function handleClick() {
+  fetch('http://localhost:3000/time/now')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    alert(`The local server time is: ${data.time}`);
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={()=>{handleClick()}}>
+      Get server time in milliseconds
+    </button>
   );
 }
 
